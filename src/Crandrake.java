5b517918d12eb55478268b3d5c595edc497d8098
Blake Romero <blake.romero724@gmail.com>
2018-02-21 13:39:07 -0600

    Began work on grading method.

--- src/Crandrake.java
+++ src/Crandrake.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 
 
 public class Crandrake extends Player {
-    
+
     public Crandrake(int color, String name) {
         super(color, name);
     }
 
     public Move getMove(BlokusBoard board) {
-
+        return null;
     }
 
+    public int grade(BlokusBoard board) {
+        int boardGrade = 0;
+        ArrayList<IntPoint> possibleMoves = board.moveLocations(getColor());
+        return 0;
+    }
+
+    public int pieceSpots(BlokusBoard board, int index){
+        return 0;
+    }
     public Player freshCopy() {
         return new BigMoverAI(getColor(), getName());
     }
